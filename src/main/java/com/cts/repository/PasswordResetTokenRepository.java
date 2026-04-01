0af30eabe88b6cbf543318b144ccd01663474afe
Sandeep <2486054@cognizant.com>
2026-04-01 16:09:04 +0530

    Add repository layer - JPA repositories for data access

---
+++ src/main/java/com/cts/repository/PasswordResetTokenRepository.java
@@ -0,0 +1,12 @@
+package com.cts.repository;
+
+import com.cts.entity.PasswordResetToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface PasswordResetTokenRepository extends JpaRepository<PasswordResetToken, Long> {
+
+    Optional<PasswordResetToken> findByToken(String token);
+    void deleteByUserUserId(Long userId);
+}
